1e322d82e687a4b20985f55d6285f18233397d34
line-fr <linefremery@gmail.com>
2024-05-22 16:23:19 +0900

    added QFT function and use it as benchmarck

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 Kate is a project to compute the ouput of quantum computer circuit using tradtionnal GPUs with the backend HIP (compatible with both NVIDIA and AMD)
 for now, there is support for multi-GPU but only on 1 node (no MPI)
-Speed needs to be measured but shows relaly good results, especially using special optimizations for preprocessing circuits.
+Speed needs to be measured but shows really good results, especially using special optimizations for preprocessing circuits.
 
 **TO COMPILE**
 hipcc main.cpp
 
-todo: implement measurment, implement new special gates, and clean code along with minor optimizations
+Current configuration: It runs a QFT over 24 qbits
+Time taken on an RTX 4050 mobile : 1081.77 ms with optimizations and 2927.04 ms without (on QFT, only GateGrouping actually does something)
+
+todo: implement measurment, implement save and load from circuit files, implement new special gates, and clean code along with minor optimizations
--- main.cpp
+++ main.cpp
@@ -10,43 +10,19 @@ using namespace std;
 
 template<typename T>
 QuantumCircuit<T> QFT(int qbitsNum){
+    QuantumCircuit<T> res(qbitsNum);
+    vector<int> qbits;
 
-}
+    for (int i = 0; i < qbitsNum; i++){
+        qbits = {i};
+        res.appendGate(Gate<T>(Hadamard, qbits));
+        for (int j = i+1; j < qbitsNum; j++){
+            qbits = {j, i}; //first element is controller which here is j
+            res.appendGate(Gate<T>(CRk, qbits, j - i +1));
+        }
+    }
 
-void matrixProdBench(){
-    hipStream_t execthread;
-    hipStreamCreate(&execthread);
-
-    auto a = createGPUMatrix<Complex<double>>(2048);
-    auto b = createGPUMatrix<Complex<double>>(2048);
-
-    matFill(a, Complex<double>(1, 0), execthread);
-    matFill(b, Complex<double>(2, 0), execthread);
-    hipStreamSynchronize(execthread);
-    
-    hipEvent_t start, end;
-    hipEventCreate(&start);
-    hipEventCreate(&end);
-
-    hipEventRecord(start, execthread);
-    auto c = matProd(a, b, execthread);
-    hipEventRecord(end, execthread);
-    hipEventSynchronize(end);
-
-    float time;
-    hipEventElapsedTime(&time, start, end);
-    cout << "the product took " << time << " ms" << endl;
-
-    Matrix<Complex<double>> cPU = c;
-    cPU.print();
-
-    hipEventDestroy(start);
-    hipEventDestroy(end);
-    hipStreamDestroy(execthread);
-
-    destroyGPUMatrix(a);
-    destroyGPUMatrix(b);
-    destroyGPUMatrix(c);
+    return res;
 }
 
 __global__ void testkernel(){
@@ -82,103 +58,28 @@ void printGpuInfo(){
     hipDeviceSynchronize();
 }
 
-void quantumOPTTest(){
-    int qbitnumber = 20;
-
-    QuantumCircuit<double> test(qbitnumber);
-    vector<int> qbits;
-    Matrix<Complex<double>> mat((1llu << 3));
-    mat.fill(Complex<double>(1., 0.));
-
-    for (int i = 0; i < qbitnumber; i++){
-        qbits = {i};
-        test.appendGate(Gate<double>(2, qbits));
-    }
-
-    for (int i = 1; i < qbitnumber; i++){
-        qbits = {0, i};
-        test.appendGate(Gate<double>(3, qbits));
-    }
-    for (int i = 1; i < qbitnumber; i++){
-        qbits = {0, i};
-        test.appendGate(Gate<double>(3, qbits));
-    }
-
-    test.print();
-
-    test.gateScheduling();
-
-    cout << endl;
-    cout << "scheduling done" << endl;
-    test.print();
-
-    test.gateFusion(6, 0.01);
-
-    cout << endl;
-    cout << "merge done!" << endl;
-    test.print();
-    //cout << "value at 00 of the last gate is : ";
-    //test.gate_set_ordered[test.gate_set_ordered.size()-1].densecontent(0, 0).print();
-    cout << endl;
-
-    test.gateGrouping(8);
-    cout << endl;
-    cout << "groups done!" << endl;
-    test.print();
-
-    test.allocate(27, 0);
-    cout << endl;
-    cout << "allocate done!" << endl;
-    test.print();
-
-    Simulator<double>(test, 1);
-
-    cout << endl;
-}
-
 int main(){
     printGpuInfo<Complex<double>>();
 
     //quantumOPTTest();
 
-    int qbitnumber = 25;
-    int gpulog2 = 0;
+    auto circuit = QFT<double>(24);
 
-    QuantumCircuit<double> test(qbitnumber);
-    vector<int> qbits;
-    Matrix<Complex<double>> mat((1llu << 5));
-    mat.fill(Complex<double>(1., 0.));
+    circuit.print();
 
-    for (int i = 0; i < qbitnumber; i+= 5){
-        qbits = {i, i+1, i+2, i+3, i+4};
-        test.appendGate(Gate<double>(mat, qbits));
-    }
+    circuit.compileOPT(5, 0.00001, 10, 0);
 
-    int l = 0;
-    vector<int> shuffled = {24, 18, 25, 26, 27, 28, 29, 30, 9, 6, 5, 17, 21, 16, 11, 0, 3, 22, 15, 23, 8, 1, 10, 20, 7, 19, 14, 12, 2, 4, 13};
-    for (const auto& i: shuffled){
-        if (i >= qbitnumber) continue;
-        qbits = {l};
-        test.appendGate(Gate<double>(2, qbits));
-        l++;
-    }
+    circuit.print();
 
-    test.gateScheduling();
-    test.gateFusion();
-    test.gateGrouping();
-    test.allocate(28, gpulog2);
-
-    test.print();
-
-    Simulator<double> sim(test, (1 << gpulog2));
+    Simulator<double> sim(circuit, 1);
 
     sim.execute(true);
-    
-    cout << "done" << endl;
 
     //todo: implement measurment, implement QFT circuit with new gates
     //better gate scheduling euristic!
     //lastly, details optimizations
+
+    cout << "done" << endl;
 
     return 0;
 }
